1b693ea46f71acd4c3a8e943949f455dd1d39398
Arom Jhee <aromjhee@gmail.com>
2020-08-03 16:57:00 -0700

    define all models except relationships

--- app/models/models.py
+++ app/models/models.py
@@ -1,4 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
+from datetime import datetime
+
 
 
 db = SQLAlchemy()
@@ -11,10 +14,123 @@ class User(db.Model):
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     image = db.Column(db.String(255))
-    email = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
     domicile_type = db.Column(db.String(100), nullable=False)
     phone_number = db.Column(db.Integer, nullable=False)
     user_info = db.Column(db.String(2000))
-    createdAt = db.Column(db.DateTime)
-    updatedAt = db.Column(db.DateTime)
+    createdAt = db.Column(
+        db.DateTime, default=datetime.utcnow)
+    updatedAt = db.Column(
+        db.DateTime(timezone=True), onupdate=func.now())
     hashed_password = db.Column(db.String(100), nullable=False)
+
+    trips = db.relationship('Trip', backref='user', lazy=True)
+    # backref means that when we query Trip, we can call Trip.user to get the user of that trip
+    # lazy=True means sqlalchemy will load all trips made by a user
+
+
+class Calendar(db.Model):
+    __tablename__ = 'calendar'
+
+    id = db.Column(db.Integer, primary_key=True)
+    start_date = db.Column(db.Date)
+    end_date = db.Column(db.Date)
+    location_id = db.Column(
+        db.Integer,
+        db.ForeignKey('locations.id'),
+        nullable=False)
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('users.id'),
+        nullable=False)
+
+
+class Trip(db.Model):
+    __tablename__ = 'trips'
+
+    id = db.Column(db.Integer, primary_key=True)
+    starting_address = db.Column(db.String(100), nullable=False)
+    starting_city = db.Column(db.String(50), nullable=False)
+    starting_state = db.Column(db.String(20), nullable=False)
+    ending_address = db.Column(db.String(100), nullable=False)
+    ending_city = db.Column(db.String(50), nullable=False)
+    ending_state = db.Column(db.String(20), nullable=False)
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('users.id'),
+        nullable=False)
+
+
+class Location(db.Model):
+    __tablename__ = 'locations'
+
+    id = db.Column(db.Integer, primary_key=True)
+    address = db.Column(db.String(100), nullable=False)
+    city = db.Column(db.String(50), nullable=False)
+    state = db.Column(db.String(20), nullable=False)
+    gps_coords = db.Column(db.String(100), nullable=False)
+    images = db.Column(db.ARRAY(db.String(255)))
+    # ARRAY types only supported in Postgres
+    website = db.Column(db.String(255))
+    description = db.Column(db.String(2000))
+    host_notes = db.Column(db.String(2000))
+    active = db.Column(db.Boolean, default=True)
+    amenity_id = db.Column(
+        db.Integer,
+        db.ForeignKey('amenities.id'),
+        nullable=False)
+    review_id = db.Column(
+        db.Integer,
+        db.ForeignKey('reviews.id'),
+        nullable=False)
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('users.id'),
+        nullable=False)
+    necessity_id = db.Column(
+        db.Integer,
+        db.ForeignKey('necessities.id'),
+        nullable=False)
+
+
+class Amenity(db.Model):
+    __tablename__ = 'amenities'
+
+    id = db.Column(db.Integer, primary_key=True)
+    electric_hookup = db.Column(db.Boolean)
+    water_hookup = db.Column(db.Boolean)
+    septic_hookup = db.Column(db.Boolean)
+    assigned_parking = db.Column(db.Boolean)
+    tow_vehicle_parking = db.Column(db.Boolean)
+    trash_removal = db.Column(db.Boolean)
+    water_front = db.Column(db.Boolean)
+    pets_allowed = db.Column(db.Boolean)
+    internet_access = db.Column(db.Boolean)
+
+
+class Necessity(db.Model):
+    __tablename__ = 'necessities'
+
+    id = db.Column(db.Integer, primary_key=True)
+    rv_compatible = db.Column(db.Boolean)
+    generators_allowed = db.Column(db.Boolean)
+    fires_allowed = db.Column(db.Boolean)
+    max_days = db.Column(db.Integer)
+    pad_type = db.Column(db.String(100))
+
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    overall_rating = db.Column(db.Integer)
+    noise = db.Column(db.Integer)
+    safety = db.Column(db.Integer)
+    cleanliness = db.Column(db.Integer)
+    access = db.Column(db.Integer)
+    site_quality = db.Column(db.Integer)
+    comments = db.Column(db.String(2000))
+    createdAt = db.Column(
+        db.DateTime, default=datetime.utcnow)
+    updatedAt = db.Column(
+        db.DateTime(timezone=True), onupdate=func.now())
