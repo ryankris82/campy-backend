1be07fc4fb60997de8033717e9215163243dda54
anusca <anna.arsentieva@gmail.com>
2020-08-03 16:34:47 -0700

    work on login

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,17 @@
 from flask import Flask
 from .config import Configuration
+from flask_login import LoginManager
+from models.User import db, User
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
+
+# TODO add routes here
+
+login = LoginManager(app)
+login.login_view = "session.login"
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
--- app/models/models.py
+++ app/models/models.py
@@ -1,10 +1,12 @@
+from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 db = SQLAlchemy()
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -18,3 +20,14 @@ class User(db.Model):
     createdAt = db.Column(db.)
     updateddAt = db.Column(db.)
     hashed_password = db.Column(db.String(100), nullable=False)
+
+    @property
+    def password(self):
+        return self.hashed_password
+
+    @password.setter
+    def password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
