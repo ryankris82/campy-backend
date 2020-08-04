27f0bff7e881bddaf3412f127ea52d9a784f5fd7
anusca <anna.arsentieva@gmail.com>
2020-08-03 17:04:05 -0700

    mockup of the users route

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
-from .config import Configuration
+from app.config import Configuration
 from flask_login import LoginManager
 from models.User import db, User
+from app.routes import session, users
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 
 # TODO add routes here
+app.register_blueprint(users.bp)
+app.register_blueprint(session.bp)
 
 login = LoginManager(app)
 login.login_view = "session.login"
