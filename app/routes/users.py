53486e01c711eec1bffe7620df9630d8afc693a4
anusca <anna.arsentieva@gmail.com>
2020-08-05 11:10:22 -0700

    add a jwt expiration variable and implement user update route

--- app/routes/users.py
+++ app/routes/users.py
@@ -16,6 +16,34 @@ def get_one_user(user_id):
 
 @bp.route("/<user_id>", methods=["PUT"])
 @jwt_required
-def update_user():
-    # TODO
-    return jsonify({"message":"update user"})
+def update_user(user_id):
+    user = User.query.get(int(user_id))
+    if user == None:
+        return jsonify({"message": "no user found for the requested id"})
+    if request.is_json:
+        image = request.json["image"]
+        password = request.json["password"]
+        phone_number = request.json["phoneNumber"]
+        user_Info = request.json["userInfo"]
+        domicile_type = request.json["domicileType"]
+        first_name = request.json["firstName"]
+        last_name = request.json["lastName"]
+    else:
+        image = request.form["image"]
+        password = request.form["password"]
+        phone_number = request.form["phoneNumber"]
+        user_Info = request.form["userInfo"]
+        domicile_type = request.form["domicileType"]
+        first_name = request.form["firstName"]
+        last_name = request.form["lastName"]
+
+    user.image = image
+    user.password = password
+    user.phone_number = phone_number
+    user.user_Info= user_Info
+    user.domicile_type = domicile_type
+    user.first_name = first_name
+    user.last_name = last_name
+    db.session.commit()
+
+    return jsonify(message="User record updated successfully.")
