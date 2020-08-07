57936ab4305fa70a922b0f9e58cee115542c3934
Aaron Bruce <aaronbruce555@gmail.com>
2020-08-07 12:49:49 -0400

    Clean up calendar.py

--- app/routes/calendar.py
+++ app/routes/calendar.py
@@ -41,70 +41,20 @@ class Calendars(Resource):
             db.session.commit()
             return {"Message": "Successfully scheduled!"}
         else:
-            # date_list = []
-            # date = start
-            # while date <= end:
-
+            # get the calendar entries for a given location
             locationDates = Calendar.query.filter_by(location_id=location_id).all()
             # get the api payload data for the requested start and end date and convert to datetime from string
             req_start_date = datetime.datetime.strptime(data["start_date"], "%Y-%m-%d").date()
             req_end_date = datetime.datetime.strptime(data["end_date"], "%Y-%m-%d").date()
-            # print("Location Dates", locationDates)
 
             for date in locationDates:
                 start = date.start_date
                 end = date.end_date
-                # print(date)
-
-                print("==========================")
-                print("Date:", date)
-                print("length of location Dates", len(locationDates))
-                print("Start: date.start_date", start)
-                print("End: date.end_date", end)
-                print("Start: req_start_date", req_start_date)
-                print("End: req_end_date", req_end_date)
-
-                        # checks if the req time is within an existing time block                                           # checks if the req time envelopes an existing time block
+                                                # checks if the req time is within an existing time block                    # checks if the req time envelopes an existing time block
                 if (req_start_date >= start and req_start_date <= end) or (req_end_date >= start and req_end_date <= end) or (req_start_date <= start and req_end_date >= end):
                     return {"Message": "Chosen date range is unavailable"}
-
+            # if the requested dates do not envelope or are enveloped by an existing date range, commit the selected dates to the database
             calendar = Calendar(**data)
             db.session.add(calendar)
             db.session.commit()
             return {"Message": "Successfully scheduled!"}
-
-            '''
-            Currently fails if requesting on a day that already has the same start and end date
-
-            '''
-
-
-        # get the calendar for the location
-
-        # if nothing found, safe to post
-        # else need to check if the dates are available if true we can post
-
-
-    #     calendar_data={
-    #         "start_date":data["start_date"],
-    #         "end_date":data["end_date"],
-    #     }
-    #     # get the dates in a range
-    #     calendar_array = []
-    #     # get the first date
-    #     new_date = data["start_date"]
-    #     while (new_date <= data["end_date"]):
-    #         # store and format the date
-    #         temp_date = new_date
-    #         # increment the date
-    #         new_date =
-    #         # push the date to the date array
-
-    #     # check if the days selected are available, (are not already in another calendar range)
-
-    #     scheduled_dates = Calendar.query.filter_by(
-    #         start_date = calendar_data["start_date"],
-    #         end_date = calendar_data["end_date"]
-    #     )
-
-    #     # if the date exists in the calendar range,
